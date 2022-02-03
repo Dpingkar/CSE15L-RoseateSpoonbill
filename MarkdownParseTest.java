f0a2990e03781e3e85a296468d3f8ca0786fb880
Dpingkar <97704377+Dpingkar@users.noreply.github.com>
2022-02-03 13:36:55 -0800

    Added a failtest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -95,4 +95,9 @@ public class MarkdownParseTest {
         ArrayList<String> expected = new ArrayList<String>();
         assertEquals(links,expected);
     }
+
+    @Test
+    public void failTest() throws IOException{
+        fail();
+    }
 }
